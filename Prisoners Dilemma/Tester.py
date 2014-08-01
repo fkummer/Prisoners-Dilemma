7732c263b3d51b7c740001a30cccf052104d24aa
Fred <fck22@cornell.edu>
2014-08-01 15:09:39 -0400

    MAD
    Added MAD ("Mutually Assured Destrcution") prisoner,

--- Prisoners Dilemma/Tester.py
+++ Prisoners Dilemma/Tester.py
@@ -4,14 +4,16 @@ import Game
 import JesusPrisoner
 import SatanPrisoner
 import Hammurabbi
+import mad
 
 JC = JesusPrisoner.JesusPrisoner()
 Lucifer = SatanPrisoner.SatanPrisoner()
 Ham = Hammurabbi.Hammurabbi()
+Sam = Hammurabbi.Hammurabbi()
+Mad = mad.MAD()
 
 
-
-x = Game.Game(Lucifer, Ham)
+x = Game.Game(Mad, Ham)
 
 x.playSet(100)
 
