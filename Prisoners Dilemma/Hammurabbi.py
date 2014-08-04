f971c6a7f8a5bcdc370bf230d825d7820e27adbc
shakemyhead <cremepuffles@gmail.com>
2014-08-03 21:28:52 -0400

    Added documention

--- Prisoners Dilemma/Hammurabbi.py
+++ Prisoners Dilemma/Hammurabbi.py
@@ -5,8 +5,10 @@ import Prisoner
 class Hammurabbi(Prisoner.Prisoner):
 
     def chooseNextMove(self):
+        #default is to play nice
         if(self.isFirstTurn()):
             return self.nice
+        #it's an eye for an eye!
         else:
             return self.getPrevOppMove()
 
